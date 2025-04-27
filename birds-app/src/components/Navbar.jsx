1bb9b3a0a70e59e1deda9895e71544b50ce6b42a
Teresa Shao <teresashao068@gmail.com>
2025-04-27 17:26:12 -0400

    update navbar color

--- birds-app/src/components/Navbar.jsx
+++ birds-app/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-
 	return (
-		<div>
-			<div className="flex gap-4 p-4 border-b items-center">
-				<Link to="/">Home</Link>
-				<Link to="/learn">Learn</Link>
-				<Link to="/quizstart">Quiz</Link>
+		<div className="bg-sky-blue sticky top-0 z-50">
+			<div className="flex justify-between items-center p-4">
+				{/* Left side */}
+				<Link to="/" className="font-semibold">HOME</Link>
+
+				{/* Right side */}
+				<div className="flex gap-8 pr-8 font-semibold">
+					<Link to="/learn" className="hover:underline">LEARN</Link>
+					<Link to="/quizstart" className="hover:underline">QUIZ</Link>
+				</div>
 			</div>
 		</div>
-
 	)
 }
 
